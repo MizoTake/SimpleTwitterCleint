ce3377951a2c40c314301278603ef0ce5841d30e
mizoguchitakeru <mizoguchitakeru@team-lab.com>
2016-09-12 15:15:58 +0900

    全体の画面遷移とプロジェクトフォルダ等の整理

---
+++ InputText/SNS/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  InputText
+//
+//  Created by 溝口 健 on 2016/09/12.
+//  Copyright © 2016年 溝口 健. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: NSObject {
+
+}
---
+++ InputText/ViewController/SendViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  InputText
+//
+//  Created by 溝口 健 on 2016/09/09.
+//  Copyright © 2016年 溝口 健. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class SendViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet private weak var textView: UITextView!
+    
+    @IBOutlet private weak var sendButton: UIBarButtonItem!
+    
+    @IBOutlet private weak var backButton: UIBarButtonItem!
+    
+    private let disposeBag = DisposeBag()
+    
+    private let MAX_CHARACTERS = 140
+    
+    private let MIN_CHARACTERS = 1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    func setup() {
+        bind()
+    }
+    
+    func bind() {
+        textView.rx_text
+            .filter{ [unowned self] in
+                $0.characters.count >= self.MAX_CHARACTERS
+            }
+            .subscribeNext{ [unowned self] _ in
+                self.textView.deleteBackward()
+            }
+            .addDisposableTo(disposeBag)
+        
+        textView.rx_text
+            .map{ [unowned self] in
+                $0.characters.count >= self.MIN_CHARACTERS
+            }
+            .subscribeNext{ [unowned self] in
+                self.sendButton.enabled = $0
+            }
+            .addDisposableTo(disposeBag)
+        
+        backButton.rx_tap
+            .subscribeNext{ [unowned self] in
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+            .addDisposableTo(disposeBag)
+    }
+}
+
